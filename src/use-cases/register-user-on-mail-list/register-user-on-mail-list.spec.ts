1dedfcaa575e26d528f0446b337cf5e944643ea5
RafaZeero <rafaa99@gmail.com>
2023-01-10 10:37:16 -0300

    feat: user data type

---
+++ src/use-cases/register-user-on-mail-list/register-user-on-mail-list.spec.ts
@@ -0,0 +1,15 @@
+import { UserData } from './user-data'
+
+describe('Register user on mailing list use case', () => {
+  test('should add user with complete data to mailing list', async () => {
+    const users: UserData[] = []
+    console.log(users)
+    // const repo: UserRepository = new InMemoryUserRepository(users);
+    // const usecase: RegisterUserOnMailingList = new RegisterUserOnMailingList(repo);
+    // const name = 'Daniel';
+    // const email = 'daniel@mail.com';
+    // const response = await usecase.RegisterUserOnMailingList({ name, email });
+    // const user = repo.findUserByEmail('daniel@mail.com');
+    // expect((await user).name).toBe('Daniel');
+  })
+})
---
+++ src/use-cases/register-user-on-mail-list/user-data.ts
@@ -0,0 +1,4 @@
+export type UserData = {
+  readonly name: string;
+  readonly email: string;
+};
