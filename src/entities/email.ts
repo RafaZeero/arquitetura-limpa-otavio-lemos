f4ae5a337667975019b2cd647c3e14647d48ee4a
RafaZeero <rafaa99@gmail.com>
2023-01-12 11:24:25 -0300

    feat: validate emails with more than 320 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,8 +1,11 @@
-type IsEmail = (email: string) => boolean
-export const isEmail: IsEmail = email => {
-  return Boolean(email)
+type IsEmailValid = (email: string) => boolean
+export const isEmailValid: IsEmailValid = email => {
+  return Boolean(email) && isLocalPartValid(email) && isEmailWithinMaxSize(email)
 }
 
 // local part is the segment of email before the '@' symbol
 type IsLocalPartValid = (email: string) => boolean
 export const isLocalPartValid: IsLocalPartValid = email => !(email.split('@')[0].length > 64)
+
+type IsEmailWithinMaxSize = (email: string) => boolean
+export const isEmailWithinMaxSize: IsEmailWithinMaxSize = email => !(email.length > 320)
