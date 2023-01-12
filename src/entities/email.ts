79d8376b429c42d55bcfdda98268a254f74eb5b6
RafaZeero <rafaa99@gmail.com>
2023-01-12 10:59:19 -0300

    feat: validate local part longer than 64 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,4 +1,8 @@
-type IsEmailValid = (email: string) => boolean
-export const isEmailValid: IsEmailValid = email => {
+type IsEmail = (email: string) => boolean
+export const isEmail: IsEmail = email => {
   return Boolean(email)
 }
+
+// local part is the segment of email before the '@' symbol
+type IsLocalPartValid = (email: string) => boolean
+export const isLocalPartValid: IsLocalPartValid = email => !(email.split('@')[0].length > 64)
