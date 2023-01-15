e50b6270604e6f333bcb3d153f51ee57f9235cc9
RafaZeero <rafaa99@gmail.com>
2023-01-15 08:43:49 -0300

    feat: validate emails with domain larger than 255 chars

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,6 +1,6 @@
 type IsEmailValid = (email: string) => boolean
 export const isEmailValid: IsEmailValid = email => {
-  return Boolean(email) && isLocalPartValid(email) && isEmailWithinMaxSize(email)
+  return Boolean(email) && isLocalPartValid(email) && isEmailWithinMaxSize(email) && isDomainWithinMaxSize(email)
 }
 
 // local part is the segment of email before the '@' symbol
@@ -9,3 +9,6 @@ const isLocalPartValid: IsLocalPartValid = email => !(email.split('@')[0].length
 
 type IsEmailWithinMaxSize = (email: string) => boolean
 const isEmailWithinMaxSize: IsEmailWithinMaxSize = email => !(email.length > 320)
+
+type IsDomainWithinMaxSize = (email: string) => boolean
+const isDomainWithinMaxSize: IsDomainWithinMaxSize = email => !(email.split('@')[1].length > 255)
