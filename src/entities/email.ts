ccecff92b2eec3e1ce6ab611117c76323ad88beb
RafaZeero <rafaa99@gmail.com>
2023-01-15 09:15:24 -0300

    feat: validate email with empty local part

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -5,7 +5,11 @@ export const isEmailValid: IsEmailValid = email => {
 
 // local part is the segment of email before the '@' symbol
 type IsLocalPartValid = (email: string) => boolean
-const isLocalPartValid: IsLocalPartValid = email => !(email.split('@')[0].length > 64)
+type IsLocalPartWithinMinSize = (email: string) => boolean
+type IsLocalPartWithinMaxSize = (email: string) => boolean
+const isLocalPartValid: IsLocalPartValid = email => isLocalPartWithinMinSize(email) && isLocalPartWithinMaxSize(email)
+const isLocalPartWithinMinSize: IsLocalPartWithinMinSize = email => email.split('@')[0].length > 0
+const isLocalPartWithinMaxSize: IsLocalPartWithinMaxSize = email => !(email.split('@')[0].length > 64)
 
 type IsEmailWithinMaxSize = (email: string) => boolean
 const isEmailWithinMaxSize: IsEmailWithinMaxSize = email => !(email.length > 320)
