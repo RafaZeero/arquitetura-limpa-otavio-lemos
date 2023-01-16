a7c43ce7747d483dbfc03fcc3c3e371036eb7da9
RafaZeero <rafaa99@gmail.com>
2023-01-16 08:10:16 -0300

    feat: validate emails with empty domain

--- src/entities/email.ts
+++ src/entities/email.ts
@@ -1,18 +1,27 @@
 type IsEmailValid = (email: string) => boolean
 export const isEmailValid: IsEmailValid = email => {
-  return Boolean(email) && isLocalPartValid(email) && isEmailWithinMaxSize(email) && isDomainWithinMaxSize(email)
+  return Boolean(email) && isLocalPartValid(email) && isDomainValid(email) && isEmailWithinMaxSize(email)
 }
-
-// local part is the segment of email before the '@' symbol
-type IsLocalPartValid = (email: string) => boolean
-type IsLocalPartWithinMinSize = (email: string) => boolean
-type IsLocalPartWithinMaxSize = (email: string) => boolean
-const isLocalPartValid: IsLocalPartValid = email => isLocalPartWithinMinSize(email) && isLocalPartWithinMaxSize(email)
-const isLocalPartWithinMinSize: IsLocalPartWithinMinSize = email => email.split('@')[0].length > 0
-const isLocalPartWithinMaxSize: IsLocalPartWithinMaxSize = email => !(email.split('@')[0].length > 64)
 
 type IsEmailWithinMaxSize = (email: string) => boolean
 const isEmailWithinMaxSize: IsEmailWithinMaxSize = email => !(email.length > 320)
 
+// local part is the segment of email before the '@' symbol
+type IsLocalPartValid = (email: string) => boolean
+const isLocalPartValid: IsLocalPartValid = email => isLocalPartWithinMinSize(email) && isLocalPartWithinMaxSize(email)
+
+type IsLocalPartWithinMinSize = (email: string) => boolean
+const isLocalPartWithinMinSize: IsLocalPartWithinMinSize = email => !(email.split('@')[0].length === 0)
+
+type IsLocalPartWithinMaxSize = (email: string) => boolean
+const isLocalPartWithinMaxSize: IsLocalPartWithinMaxSize = email => !(email.split('@')[0].length > 64)
+
+// domain is the segment of email after the '@' symbol
+type IsDomainValid = (email: string) => boolean
+const isDomainValid: IsDomainValid = email => isDomainWithinMinSize(email) && isDomainWithinMaxSize(email)
+
 type IsDomainWithinMaxSize = (email: string) => boolean
 const isDomainWithinMaxSize: IsDomainWithinMaxSize = email => !(email.split('@')[1].length > 255)
+
+type IsDomainWithinMinSize = (email: string) => boolean
+const isDomainWithinMinSize: IsDomainWithinMinSize = email => !(email.split('@')[1].length === 0)
