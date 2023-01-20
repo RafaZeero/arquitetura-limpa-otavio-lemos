af4b64a80be0b8d6139f888fc53f32a358b8f787
RafaZeero <rafaa99@gmail.com>
2023-01-20 12:03:40 -0300

    refactor(tests): change it to functional paradigm

--- src/use-cases/register-user-on-mail-list/repository/in-memory-user-repository.spec.ts
+++ src/use-cases/register-user-on-mail-list/repository/in-memory-user-repository.spec.ts
@@ -1,11 +1,11 @@
 import { UserData } from '../user-data'
-import { InMemoryUserRepository } from './in-memory-user-repository'
+import { add, findAllUsers, findUserByEmail, _repository } from './in-memory-user-repository'
 
 describe('In Memory User repository', () => {
   test('should return null if user is not found', async () => {
     const users: UserData[] = []
-    const sut = new InMemoryUserRepository(users)
-    const user = await sut.findUserByEmail('user@mail.co')
+    _repository.push(...users)
+    const user = await findUserByEmail('user@mail.co')
     expect(user).toBeNull()
   })
 
@@ -13,19 +13,28 @@ describe('In Memory User repository', () => {
     const users: UserData[] = []
     const name = 'user_name'
     const email = 'user@mail.co'
-    const sut = new InMemoryUserRepository(users)
-    await sut.add({ name, email })
-    const user = await sut.findUserByEmail('user@mail.co')
+    _repository.push(...users)
+    await add({ name, email })
+    const user = await findUserByEmail('user@mail.co')
     expect(user.name).toBe('user_name')
   })
 
   test('should return all users in the repository', async () => {
+    clearRepo()
     const users: UserData[] = [
       { name: 'user_name1', email: 'user1@mail.co' },
       { name: 'user_name2', email: 'user2@mail.co' }
     ]
-    const sut = new InMemoryUserRepository(users)
-    const usersInRepository = sut.findAllUsers()
+    _repository.push(...users)
+    const usersInRepository = findAllUsers()
     expect((await usersInRepository).length).toBe(users.length)
   })
 })
+
+const clearRepo = () => {
+  if (_repository.length === 0) return
+
+  _repository.pop()
+
+  clearRepo()
+}
