4ab5f9603cffdf1dbc5e6ebbedaf1259b13cbd8b
RafaZeero <rafaa99@gmail.com>
2023-08-01 22:00:14 -0300

    feat: add create name to create user function

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -3,13 +3,14 @@ import { createEmail } from './email';
 import { InvalidUserError } from './errors/invalid-email-error';
 import { UserData } from './user-data';
 import { pipe } from 'fp-ts/lib/function';
+import { createName } from './name';
 
 type User = UserData;
 
 type CreateUser = (user: User) => E.Either<InvalidUserError, User>;
-export const createUser: CreateUser = user => {
-  return pipe(
+export const createUser: CreateUser = user =>
+  pipe(
     createEmail(user.email),
-    E.map((email): User => ({ email, name: user.name }))
+    E.bindTo('email'),
+    E.bind('name', () => createName(user.name))
   );
-};
